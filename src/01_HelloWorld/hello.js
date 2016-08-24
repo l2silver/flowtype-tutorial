1bf9fadde0eed80daa8f7fb84fe8f2aa66e60e60
Leigh Silverstein <leighsilverstein@Leighs-MacBook-Pro.local>
2016-08-24 09:48:43 -0400

    remove expletive content

--- src/01_HelloWorld/hello.js
+++ src/01_HelloWorld/hello.js
@@ -1,4 +1,17 @@
-/* @flow */
+//@flow
+
+
+/*
+BASIC TYPES
+
+number
+string
+Array<> | number[]
+Object | {hello: string} | {[key: string]: string}
+Function | <U>()=>U
+null
+*/
+
 
 function foo(x) {
   return x * 10;
